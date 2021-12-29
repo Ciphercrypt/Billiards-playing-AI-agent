ec226164f05b6f6548e929adc5729e94b00c6577
Avishkar Mohite <avimohite2019@gmail.com>
2021-12-29 18:14:56 +0530

    trailblazer commit

---
+++ script/Global.js
@@ -0,0 +1,19 @@
+const LOG = true;
+
+const BALL_SIZE = 38;
+const BORDER_SIZE = 57;
+const HOLE_RADIUS = 46;
+
+const DELTA = 1/100;
+
+let DISPLAY = true;
+let SOUND_ON = true;
+let GAME_STOPPED = true;
+
+let KEYBOARD_INPUT_ON = true;
+
+let TRAIN_ITER = 100;
+let AI_ON = true;
+let AI_PLAYER_NUM = 1;
+let DISPLAY_TRAINING = false;
+
